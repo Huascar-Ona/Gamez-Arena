981035b267788bbfa46b934dd1b6115c331937d1
Huascar Ona <huascarking@hotmail.com>
2012-01-24 18:21:45 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Sistema de partidas igualadas.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamezArena::Application.config.secret_token = '504bfc52d72e1c30f94d762017bfaca3ab3ca40e05910689fec0b2917333ec175c2538fbc67db5ae4dfaf55c9f439abc1e052ce2d553c2a94d13c6083c6b58a1'
